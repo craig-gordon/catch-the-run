2b81a007e967acc8634790e85c24d8223a9910d0
Craig Gordon <cyghfer@gmail.com>
2019-11-03 16:54:40 -0500

    Add more createSub / processEvent logic

--- eventProcessing/processEvent.js
+++ eventProcessing/processEvent.js
@@ -1,37 +1,78 @@
-'use strict';
+"use strict";
 
-const DynamoDB = require('aws-sdk/clients/dynamodb');
-
+const DynamoDB = require("aws-sdk/clients/dynamodb");
 const dynamoClient = new DynamoDB.DocumentClient();
+const axios = require("axios").default;
+
+const webPushService = require("web-push");
+
+webPushService.setVapidDetails(
+  "https://catchthe.run/",
+  "BJMQ-CpMM_-OoO2hNPt3oM_pM8TSpPhSL_rGwGix99iLj0hdyHCZTP3XYeOO8sf9ghhHHFO2I7QpDrgSyGd1VQ4",
+  "S_3sQfZvYAh7LGuKkXreV_kfcmAkdNFL0nAoI2z1P3w"
+);
 
 module.exports.processEvent = async (event, context) => {
-    console.log('Lambda Context:', context);
-    console.log('Create Feed Request:', event);
+  console.log("Lambda Context:", context);
+  console.log("Create Feed Request:", event);
 
-    const params = {
-        TableName: 'Main',
-        KeyConditionExpression: 'PRT = :PRT and begins_with (SRT, :SRT)',
-        ExpressionAttributeValues: {
-            ':PRT': event.ProviderName,
-            ':SRT': 'F|SUB'
-        }
+  const params = {
+    TableName: "Main",
+    KeyConditionExpression: "PRT = :PRT and begins_with (SRT, :SRT)",
+    ExpressionAttributeValues: {
+      ":PRT": event.ProviderName,
+      ":SRT": "F|SUB"
+    }
+  };
+
+  try {
+    const getAllSubsResponse = await dynamoClient.query(params).promise();
+    console.log("all subs:", getAllSubsResponse);
+
+    const subs = getAllSubsResponse.Items;
+
+    for (let i = 0; i < subs.length; i++) {
+      const sub = subs[i];
+
+      if (sub["WEBPUSH"]) {
+        webPushService
+          .sendNotification(JSON.parse(sub.WEBPUSH.Endpoint), event.Message)
+          .then(res => console.log("webpush res:", res))
+          .catch(err => console.log("webpush error:", err));
+      }
+      if (sub["DISCORD-AT"]) {
+        axios.post(
+          `https://discordapp.com/api/channels/${sub["DISCORD-AT"].DiscordServerId}/messages`,
+          {
+            content: "@" + sub["DISCORD-AT"].Endpoint,
+            embed: {
+              description: event.Message,
+              color: 15155743
+            }
+          },
+          {
+            headers: {
+              Authorization:
+                "Bot NjM1NTc3MjkxNzQ4OTMzNzAz.XazGIQ.O4R5GiMXZNHLAs_ClPhOlIR99Zc",
+              "Content-Type": "application/json"
+            }
+          }
+        );
+      }
     }
 
-    try {
-        const getAllSubsResponse = await dynamoClient.query(params).promise();
-        console.log('all subs:', getAllSubsResponse);
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            }
-        }
-    } catch (e) {
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            }
-        }
-    }
-}
+    // return {
+    //   statusCode: 200,
+    //   headers: {
+    //     "Access-Control-Allow-Origin": "*"
+    //   }
+    // };
+  } catch (e) {
+    // return {
+    //   statusCode: 200,
+    //   headers: {
+    //     "Access-Control-Allow-Origin": "*"
+    //   }
+    // };
+  }
+};
--- subscriptionManagement/createSubscription.js
+++ subscriptionManagement/createSubscription.js
@@ -1,70 +1,71 @@
-'use strict';
+"use strict";
 
-const DynamoDB = require('aws-sdk/clients/dynamodb');
-
-const dynamoClient = new DynamoDB.DocumentClient();
+const dynamoDB = require("aws-sdk/clients/dynamodb");
+const dynamoClient = new dynamoDB.DocumentClient();
 
 module.exports.createSubscription = async (event, context) => {
-    console.log('Lambda Context:', context);
-    console.log('Create Subscription Request:', event);
+  console.log("Lambda Context:", context);
+  console.log("Create Subscription Request:", event);
 
-    const params = {
-        TableName: 'Main',
-        Key: {
-            PRT: event.ProviderName,
-            SRT: `F|SUB|${event.ConsumerName}`
-        }
+  const newSubInstance = {
+    Endpoint: event.Endpoint,
+    DiscordServerId: event.DiscordServerId,
+    FilterPolicy: event.FilterPolicy
+  };
+
+  const params = {
+    TableName: "Main",
+    Key: {
+      PRT: event.ProviderName,
+      SRT: `F|SUB|${event.ConsumerName}`
     }
+  };
 
-    let getSubResponse;
+  let getSubResponse;
+
+  try {
+    getSubResponse = await dynamoClient.get(params).promise();
+  } catch (e) {
+    console.log("error:", e);
+  }
+
+  if (getSubResponse.Item) {
+    const existingSub = getSubResponse.Item;
+    existingSub[event.Protocol] = newSubInstance;
 
     try {
-        getSubResponse = await dynamoClient.get(params).promise();
+      const addSubInstanceResponse = await dynamoClient
+        .put({ TableName: "Main", Item: existingSub })
+        .promise();
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      };
     } catch (e) {
-        console.log('error:', e);
+      console.log("error:", e);
     }
+  } else {
+    const newSub = {
+      PRT: event.ProviderName,
+      SRT: `F|SUB|${event.ConsumerName}`,
+      G1S: event.ProviderName
+    };
+    newSub[event.Protocol] = newSubInstance;
 
-    if (getSubResponse.Item) {
-        const existingSub = getSubResponse.Item;
-        existingSub[event.Protocol] = {
-            Endpoint: event.Endpoint,
-            DiscordServerId: event.DiscordServerId,
-            FilterPolicy: event.FilterPolicy
+    try {
+      const addNewSubResponse = await dynamoClient
+        .put({ TableName: "Main", Item: newSub })
+        .promise();
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*"
         }
-
-        try {
-            const addSubInstanceResponse = await dynamoClient.put({ TableName: 'Main', Item: existingSub }).promise();
-            return {
-                statusCode: 200,
-                headers: {
-                    'Access-Control-Allow-Origin': '*'
-                }
-            }
-        } catch (e) {
-            console.log('error:', e);
-        }
-    } else {
-        const newSub = {
-            PRT: event.ProviderName,
-            SRT: `F|SUB|${event.ConsumerName}`,
-            G1S: event.ProviderName
-        };
-        newSub[event.Protocol] = {
-            Endpoint: event.Endpoint,
-            DiscordServerId: event.DiscordServerId,
-            FilterPolicy: event.FilterPolicy
-        }
-
-        try {
-            const addNewSubResponse = await dynamoClient.put({ TableName: 'Main', Item: newSub }).promise();
-            return {
-                statusCode: 200,
-                headers: {
-                    'Access-Control-Allow-Origin': '*'
-                }
-            }
-        } catch (e) {
-            console.log('error:', e);
-        }
+      };
+    } catch (e) {
+      console.log("error:", e);
     }
-}
+  }
+};
